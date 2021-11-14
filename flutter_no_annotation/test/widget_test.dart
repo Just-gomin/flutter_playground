e85927ac2305ebe6a2341993352af0a0f26db731
안강민 <suptizen@fatoscorp.com>
2021-11-15 07:49:27 +0900

    Flutter 빈프로젝트 수정. 주석 제거 및 pubspec 수정

--- flutter_no_annotation/test/widget_test.dart
+++ flutter_no_annotation/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prac_flutter_provider/main.dart';
+import 'package:flutter_no_annotation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
